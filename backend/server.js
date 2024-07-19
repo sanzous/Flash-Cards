5036f309dd7eeea26da9af4b702fa428c85b62ae
sanzous <canastraleonard123@gmail.com>
2024-07-19 17:39:06 -0500

    Express, DotEnv Setup complete

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,23 @@
+require('dotenv').config()
 const express = require('express')
 
+// initliatize express app
 const app = express()
 
-app.listen(8000, () => {
-    console.log('Listening on port 8000')
+
+// middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+
+//routes
+app.get('/', (req,res) => {
+    res.json({mssg: 'Welcome to the app'})
+})
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`)
 })
